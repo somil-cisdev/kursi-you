318f2d01f205c43ce219dfbdada6a02648eb5d45
vikrantcisdev <vikrant.raj@cisinlabs.com>
2025-07-28 18:38:17 +0530

    redo swiper.min.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,3 @@
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
   const togglePassword = document.getElementById("togglePassword");
   const ConfirmtogglePassword = document.getElementById("togglePasswordConfirm");
